a5e5270e0bdf316f1f9159b03a59fb225c217931
johnw721 <johnwalker721@gmail.com>
2022-09-07 13:56:11 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import "./styles.css";
-import Nav from "../components/Nav";
+import Nav from "./components/Nav";
 import { React, Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
+        <Nav/>
         <h2>Start editing to see some magic happen!</h2>
       </div>
     );
